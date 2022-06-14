cd87d527e052a60f1408a5f838b222800a5a69f2
Albert Chang <a89529294@hotmail.com>
2022-06-14 16:02:29 +0800

    make timer editable

--- ch1-pomodoro/src/App.tsx
+++ ch1-pomodoro/src/App.tsx
@@ -1,23 +1,58 @@
-import React, { useEffect, useState } from "react";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import "./App.css";
 
 import gear from "./images/gear.svg";
 
-const TimerDisplay = ({ time }: { time: number }) => {
+const Input = ({
+  value,
+  onChange,
+}: {
+  value: string | number;
+  onChange: (arg: any) => void;
+}) => (
+  <input
+    value={value}
+    className="w-full text-center bg-transparent outline-none"
+    onChange={onChange}
+  />
+);
+
+const TimerDisplay = ({
+  time,
+  setTime,
+}: {
+  time: number;
+  setTime: Dispatch<SetStateAction<number>>;
+}) => {
   const minRaw = Math.floor(time / 60);
   const secRaw = time % 60;
   const min = minRaw < 10 ? "0" + minRaw : minRaw;
   const sec = secRaw < 10 ? "0" + secRaw : secRaw;
+
   return (
     <div className="flex justify-center w-2/3 text-white text-7xl h-fit">
-      <span>{min}</span>:<span>{sec}</span>
+      <Input
+        value={min}
+        onChange={(e) => {
+          const newMin = +e.target.value < 100 ? +e.target.value : 99;
+          setTime(newMin * 60 + secRaw);
+        }}
+      />
+      :
+      <Input
+        value={sec}
+        onChange={(e) => {
+          const newSec = +e.target.value < 60 ? +e.target.value : 59;
+          setTime(minRaw * 60 + newSec);
+        }}
+      />
     </div>
   );
 };
 
 function App() {
   const [isRunning, setIsRunning] = useState(false);
-  const [time, setTime] = useState(12);
+  const [time, setTime] = useState(900);
   useEffect(() => {
     const id =
       isRunning &&
@@ -32,7 +67,7 @@ function App() {
       <div className="grid bg-green-700 rounded-full w-80 aspect-square place-content-center">
         <div className="w-[19rem] bg-dark aspect-square rounded-full flex flex-col items-center">
           <div className="flex-1"></div>
-          <TimerDisplay time={time} />
+          <TimerDisplay time={time} setTime={setTime} />
           <div className="flex flex-col items-center justify-center flex-1 gap-3">
             <h3
               className="text-3xl text-white opacity-50 cursor-pointer"
