b6f3633c72e310148b8f07f5c3fc9c2bb8f30334
Albert Chang <a89529294@hotmail.com>
2022-06-15 16:01:48 +0800

    make input display more robust

--- ch1-pomodoro/src/App.tsx
+++ ch1-pomodoro/src/App.tsx
@@ -3,48 +3,74 @@ import "./App.css";
 
 import gear from "./images/gear.svg";
 
-const Input = ({
-  value,
-  onChange,
-}: {
-  value: string | number;
-  onChange: (arg: any) => void;
-}) => (
+interface InputProps
+  extends React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {}
+
+const populateNumber = (num: number) => (num < 10 ? "0" + num : num);
+const sanitizeInput = (value: string) => {
+  return value === "" || (value.startsWith("0") && !Number.isNaN(+value))
+    ? value
+    : parseInt(value);
+};
+
+const Input = ({ value, onChange, onClick }: InputProps) => (
   <input
     value={value}
     className="w-full text-center bg-transparent outline-none"
     onChange={onChange}
+    onClick={onClick}
   />
 );
 
 const TimerDisplay = ({
   time,
   setTime,
+  isRunning,
+  setIsRunning,
 }: {
   time: number;
   setTime: Dispatch<SetStateAction<number>>;
+  isRunning: boolean;
+  setIsRunning: Dispatch<SetStateAction<boolean>>;
 }) => {
   const minRaw = Math.floor(time / 60);
   const secRaw = time % 60;
-  const min = minRaw < 10 ? "0" + minRaw : minRaw;
-  const sec = secRaw < 10 ? "0" + secRaw : secRaw;
+  const min = populateNumber(minRaw);
+  const sec = populateNumber(secRaw);
+
+  const [editMin, setEditMin] = useState(min);
+  const [editSec, setEditSec] = useState(sec);
+
+  useEffect(() => {
+    isRunning && setEditMin(min);
+    isRunning && setEditSec(sec);
+  }, [min, sec, isRunning]);
 
   return (
     <div className="flex justify-center w-2/3 text-white text-7xl h-fit">
       <Input
-        value={min}
+        value={isRunning ? min : editMin}
         onChange={(e) => {
-          const newMin = +e.target.value < 100 ? +e.target.value : 99;
-          setTime(newMin * 60 + secRaw);
+          let value = sanitizeInput(e.target.value);
+          if (Number.isNaN(value) || value > 99) return;
+          setEditMin(value);
+          setTime(+value * 60 + secRaw);
         }}
+        onClick={() => setIsRunning(false)}
       />
       :
       <Input
-        value={sec}
+        value={isRunning ? sec : editSec}
         onChange={(e) => {
-          const newSec = +e.target.value < 60 ? +e.target.value : 59;
-          setTime(minRaw * 60 + newSec);
+          let value = sanitizeInput(e.target.value);
+          if (Number.isNaN(value) || value > 99) return;
+          setEditSec(value);
+          setTime(+value + minRaw * 60);
         }}
+        onClick={() => setIsRunning(false)}
       />
     </div>
   );
@@ -57,7 +83,7 @@ function App() {
     const id =
       isRunning &&
       setInterval(() => {
-        setTime((t) => --t);
+        setTime((t) => (t > 0 ? --t : 0));
       }, 1000);
     return () => clearInterval(typeof id === "number" ? id : 0);
   }, [isRunning]);
@@ -67,7 +93,12 @@ function App() {
       <div className="grid bg-green-700 rounded-full w-80 aspect-square place-content-center">
         <div className="w-[19rem] bg-dark aspect-square rounded-full flex flex-col items-center">
           <div className="flex-1"></div>
-          <TimerDisplay time={time} setTime={setTime} />
+          <TimerDisplay
+            time={time}
+            setTime={setTime}
+            isRunning={isRunning}
+            setIsRunning={setIsRunning}
+          />
           <div className="flex flex-col items-center justify-center flex-1 gap-3">
             <h3
               className="text-3xl text-white opacity-50 cursor-pointer"
