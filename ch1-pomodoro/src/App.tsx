ee4de969ae49c53845a781352a8cee81739689d7
Albert Chang <a89529294@hotmail.com>
2022-06-16 16:39:47 +0800

    need to refactor the timer logic

--- ch1-pomodoro/src/App.tsx
+++ ch1-pomodoro/src/App.tsx
@@ -12,7 +12,7 @@ interface InputProps
 const populateNumber = (num: number) => (num < 10 ? "0" + num : num);
 const sanitizeInput = (value: string) => {
   return value === "" || (value.startsWith("0") && !Number.isNaN(+value))
-    ? value
+    ? value.substring(0, 2)
     : parseInt(value);
 };
 
@@ -30,16 +30,20 @@ const TimerDisplay = ({
   setTime,
   isRunning,
   setIsRunning,
+  setIsEditing,
 }: {
   time: number;
   setTime: Dispatch<SetStateAction<number>>;
   isRunning: boolean;
   setIsRunning: Dispatch<SetStateAction<boolean>>;
+  setIsEditing: Dispatch<SetStateAction<boolean>>;
 }) => {
   const minRaw = Math.floor(time / 60);
   const secRaw = time % 60;
   const min = populateNumber(minRaw);
   const sec = populateNumber(secRaw);
+
+  console.log(secRaw, sec);
 
   const [editMin, setEditMin] = useState(min);
   const [editSec, setEditSec] = useState(sec);
@@ -48,6 +52,11 @@ const TimerDisplay = ({
     isRunning && setEditMin(min);
     isRunning && setEditSec(sec);
   }, [min, sec, isRunning]);
+
+  function editTimer() {
+    setIsRunning(false);
+    setIsEditing(true);
+  }
 
   return (
     <div className="flex justify-center w-2/3 text-white text-7xl h-fit">
@@ -59,7 +68,7 @@ const TimerDisplay = ({
           setEditMin(value);
           setTime(+value * 60 + secRaw);
         }}
-        onClick={() => setIsRunning(false)}
+        onClick={editTimer}
       />
       :
       <Input
@@ -70,7 +79,7 @@ const TimerDisplay = ({
           setEditSec(value);
           setTime(+value + minRaw * 60);
         }}
-        onClick={() => setIsRunning(false)}
+        onClick={editTimer}
       />
     </div>
   );
@@ -78,7 +87,10 @@ const TimerDisplay = ({
 
 function App() {
   const [isRunning, setIsRunning] = useState(false);
-  const [time, setTime] = useState(900);
+  const [time, setTime] = useState(3);
+  const [isDone, setIsDone] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
   useEffect(() => {
     const id =
       isRunning &&
@@ -88,9 +100,24 @@ function App() {
     return () => clearInterval(typeof id === "number" ? id : 0);
   }, [isRunning]);
 
+  useEffect(() => {
+    if (time === 0) {
+      setIsRunning(false);
+      setIsDone(true);
+      !isEditing &&
+        setTimeout(() => {
+          alert("Time's up!");
+        }, 100);
+    } else setIsDone(false);
+  }, [time, isEditing]);
+
   return (
     <div className="grid min-h-full bg-dark place-content-center">
-      <div className="grid bg-green-700 rounded-full w-80 aspect-square place-content-center">
+      <div
+        className={`grid rounded-full w-80 aspect-square place-content-center ${
+          isDone ? "bg-red-600" : "bg-green-700"
+        }`}
+      >
         <div className="w-[19rem] bg-dark aspect-square rounded-full flex flex-col items-center">
           <div className="flex-1"></div>
           <TimerDisplay
@@ -98,13 +125,17 @@ function App() {
             setTime={setTime}
             isRunning={isRunning}
             setIsRunning={setIsRunning}
+            setIsEditing={setIsEditing}
           />
           <div className="flex flex-col items-center justify-center flex-1 gap-3">
             <h3
               className="text-3xl text-white opacity-50 cursor-pointer"
-              onClick={() => setIsRunning((r) => !r)}
+              onClick={() => {
+                setIsRunning((r) => !r);
+                setIsEditing(false);
+              }}
             >
-              {isRunning ? "PAUSE" : "START"}
+              {isRunning || isDone ? "PAUSE" : "START"}
             </h3>
             <img src={gear} className="cursor-pointer w-9 opacity-30" />
           </div>
