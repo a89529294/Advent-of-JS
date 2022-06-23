2f9bb8fb3c21d6447fbc6eb38547def6c294e014
Albert Chang <a89529294@hotmail.com>
2022-06-23 16:30:18 +0800

    start working on ch3

---
+++ ch3-piano-keyboard/src/App.tsx
@@ -0,0 +1,11 @@
+import Keyboard from "./components/Keyboard";
+
+function App() {
+  return (
+    <div className="bg-[#37B2C3] min-h-full grid place-items-center">
+      <Keyboard className="w-9/12" />
+    </div>
+  );
+}
+
+export default App;
