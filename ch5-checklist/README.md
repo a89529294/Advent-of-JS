2864c6e7146b6592245d16dd623d0376ab2c1e58
Albert Chang <a89529294@hotmail.com>
2022-06-27 12:35:23 +0800

    finish ch5

--- ch5-checklist/README.md
+++ ch5-checklist/README.md
@@ -4,8 +4,22 @@
 2.  immeadiately after the input element add a `label` element
 3.  use the custom image as the bg image of the `label` element
 4.  use adjacent sibling combinator (+) to change the bg image of the `label` element whenever `input` is checked
-    ```
+    ```js
         input:checked + label {
             bg:url(....)
         }
     ```
+
+- use `e.shiftKey` boolean property to see if the shift key was pressed in combination with any other key. `e` can be any event, not just keyboard events.
+
+```js
+    if (e.shiftKey && e.code = '...') {
+        ...
+    }
+```
+
+- functionalities
+
+1. click to strike through
+2. shift click to strike through multiple items
+3. tab + space/enter to strike through
--- ch5-checklist/src/App.tsx
+++ ch5-checklist/src/App.tsx
@@ -8,17 +8,38 @@ const topics = [
   "4  ||  Tech Behind the Podcast",
   "5 || Tech Behind SelfTeach.me",
   "6 || Freelancing (Part 1)",
+  "7 || Freelancing (Part 2)",
+  "8 || The Tech Behind jamesqquick.com",
+  "9 || The Tech Behind SelfTeach.me",
+  "10 || Tech Behind SelfTeach.me",
 ];
 
 function App() {
   const [checkedBoxes, setCheckedBoxes] = useState(
     new Array(topics.length).fill(false)
   );
+  const [lastCheckBoxIndexSetToTrue, setLastCheckBoxIndexSetToTrue] =
+    useState(-1);
 
-  function toggleCheckBox(i: number) {
+  function toggleCheckBox(i: number, isShiftPressed: boolean) {
     setCheckedBoxes((cbs) => {
-      const copy = cbs.slice();
-      copy.splice(i, 1, !cbs[i]);
+      let copy = cbs.slice();
+      !cbs[i] && setLastCheckBoxIndexSetToTrue(i);
+      cbs[i] && setLastCheckBoxIndexSetToTrue(-1);
+      if (!isShiftPressed) {
+        copy.splice(i, 1, !cbs[i]);
+      } else {
+        copy = copy.map((bool, j) =>
+          i > lastCheckBoxIndexSetToTrue
+            ? j > lastCheckBoxIndexSetToTrue && j <= i
+              ? !bool
+              : bool
+            : j >= i && j < lastCheckBoxIndexSetToTrue
+            ? !bool
+            : bool
+        );
+      }
+
       return [...copy];
     });
   }
@@ -29,24 +50,27 @@ function App() {
         src={cover}
         className="w-[550px] aspect-square drop-shadow-primary relative z-10"
       />
-      <div className="bg-white w-[600px] aspect-[1.2/1] rounded-r-3xl drop-shadow-secondary pt-14 pl-10 grid gap-8 content-start font-primary">
+      <div className="bg-white w-[600px] aspect-[1.2/1] rounded-r-3xl drop-shadow-secondary py-14 pl-10 grid gap-8 content-start font-primary overflow-auto">
         <h1 className="text-xl text-[#A7A7A7] font-black tracking-wider">
           LISTEN TO ALL THE COMPRESSED.FM EPISODES
         </h1>
-        <ul className="text-3xl text-[#4E4E4E] grid gap-6">
+        <ul className="text-2xl text-[#4E4E4E] grid gap-6">
           {topics.map((topic, i) => (
             <li
               key={i}
-              className="flex items-center gap-5"
+              className={`flex cursor-pointer items-center gap-5 ${
+                checkedBoxes[i] ? "line-through" : ""
+              }`}
               onKeyDown={(e) => {
                 if (
                   e.code === "Space" ||
                   (e.code === "Enter" && e.target instanceof HTMLLabelElement)
                 ) {
                   e.preventDefault();
-                  toggleCheckBox(i);
+                  toggleCheckBox(i, e.shiftKey);
                 }
               }}
+              onClick={(e) => toggleCheckBox(i, e.shiftKey)}
             >
               <input
                 type="checkbox"
@@ -56,11 +80,10 @@ function App() {
                 onChange={() => {}}
               />
               <label
-                className="inline-block w-10 bg-[length:90%_90%] bg-no-repeat bg-center aspect-square bg-unchecked-checkbox peer-checked:bg-checked-checkbox focus-visible:outline-none focus-visible:shadow-[inset_1px_1px_2px_3px_rgba(25_29_50_/_0.2)] rounded-2xl"
+                className="cursor-pointer inline-block w-10 bg-[length:90%_90%] bg-no-repeat bg-center aspect-square bg-unchecked-checkbox peer-checked:bg-checked-checkbox focus-visible:outline-none focus-visible:shadow-[inset_1px_1px_2px_3px_rgba(25_29_50_/_0.2)] rounded-2xl"
                 htmlFor={`box-${i}`}
                 tabIndex={i + 1}
                 data-id={i}
-                onClick={() => toggleCheckBox(i)}
               />
               {topic}
             </li>
