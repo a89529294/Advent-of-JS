26923695d2e167f5668b829b7c9f99fdce5c5e93
Albert Chang <a89529294@hotmail.com>
2022-07-18 16:11:52 +0800

    add bottom img slider

--- ch9-img-carousel/src/App.tsx
+++ ch9-img-carousel/src/App.tsx
@@ -7,6 +7,7 @@ function App() {
       <Sidebar dir="left" />
       <div className="flex-1"></div>
       <Sidebar dir="right" />
+      <Slider />
     </div>
   );
 }
@@ -17,9 +18,35 @@ function Sidebar({ dir }: { dir: "left" | "right" }) {
       <img
         src={dir === "left" ? leftChevron : rightChevron}
         alt="left chevron"
-      ></img>
+      />
     </div>
   );
 }
+
+function Slider() {
+  return (
+    <div className="absolute bottom-0  p-5">
+      <div className="flex overflow-x-scroll no-scrollbar gap-7">
+        {imgs.map((m, i) => (
+          <img key={i} src={m} className="w-44 aspect-square object-cover" />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const imgs = [
+  "/dave-hoefler-okUIdo6NxGo-unsplash.jpg",
+  "eugene-golovesov-EXdXp7Z4X4w-unsplash.jpg",
+  "finding-dan-dan-grinwis-O35rT6OytRo-unsplash.jpg",
+  "jakob-owens-EwRM05V0VSI-unsplash.jpg",
+  "jennifer-reynolds-_8HI5xf4TkE-unsplash.jpg",
+  "kellen-riggin-SIBOiXKg0Ds-unsplash.jpg",
+  "rafael-hoyos-weht-zhkAp8DGkxw-unsplash.jpg",
+  "sherman-yang-VBBGigIuaDY-unsplash.jpg",
+  "silas-baisch-Wn4ulyzVoD4-unsplash.jpg",
+  "sonya-romanovska-wzdXhyi3AiM-unsplash.jpg",
+  "vincentiu-solomon-ln5drpv_ImI-unsplash.jpg",
+];
 
 export default App;
