ae48b79f658412429b500252742f9407b112826f
Albert Chang <a89529294@hotmail.com>
2022-07-26 17:05:27 +0800

    start on UI

--- ch10-verification-code-form/src/App.tsx
+++ ch10-verification-code-form/src/App.tsx
@@ -3,7 +3,33 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="bg-[#E3E9F9] min-h-full grid place-items-center font-pro">
+      <form className="bg-white rounded-lg pt-16 pb-7 px-8 grid gap-11 text-center">
+        <h1 className="text-purple-primary font-black text-xl uppercase">
+          Authorization CODE
+        </h1>
+        <h2 className="text-xl text-[#737580] -mt-8">
+          Please enter the code that we sent to (***) *** - 2819.
+        </h2>
+        <div className="flex justify-center gap-5">
+          <InputBox />
+          <InputBox />
+          <InputBox />
+          <InputBox />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+function InputBox() {
+  return (
+    <input
+      type="number"
+      className="w-20 aspect-square bg-[#F0F3FA] rounded-md shadow-[inset_0_0_4px_rgba(0,0,0,0.05)] text-purple-primary text-[60px] font-black text-center"
+    />
+  );
 }
 
 export default App;
--- ch10-verification-code-form/tailwind.config.cjs
+++ ch10-verification-code-form/tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pro: ["'Source Sans Pro'", "sans-serif"],
+      },
+      colors: {
+        "purple-primary": "#6778E8",
+      },
+    },
   },
   plugins: [],
 };
